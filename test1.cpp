bcf554ec3db1d17ab012f53a38af2d31d2f27dfe
Edgar Apaza <edgarapazac@gmail.com>
2018-05-07 18:28:07 -0500

    Agregar parametro suma

--- test1.cpp
+++ test1.cpp
@@ -9,7 +9,11 @@ int main(int argc, char const *argv[])
 	int a = 3;
 	int b = 5;
 
-	cout<<"valor A"<<a;
-	cout<<"valor B"<<b;
+	cout<<"valor A"<<a<<endl;
+	cout<<"valor B"<<b<<endl;
+	//Sumar los valores
+	int suma = a + b;
+	cout<<"La suma ES"<<suma<<endl;
+
 	return 0;
 }
